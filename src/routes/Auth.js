b16ff010341c74494b81bec6c54ff399a498d295
dev-cmkim <cm971003@gmail.com>
2022-05-16 23:11:07 +0900

    로그인 폼 생성 및 이벤트구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
+// facebook
+// https://cwitter-b2e19.firebaseapp.com/__/auth/handler
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e) => {
+        const {target: {name, value}} = e;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+        console.log(e.target.name)
+    }
+    const onSubmit = (e) => {
+       e.preventDefault();
+       console.log("로그인 계정:", email, password)
+
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email"
+                    type="email" 
+                    placeholder="Email" 
+                    value={email}
+                    onChange={onChange}
+                    required/>    
+                <input 
+                    name="password"
+                    type="password" 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={onChange}
+                    required/>    
+                <input type="submit" value="Log In"/>    
+            </form>    
+            <div>
+                <button> Continue with Goggle</button>
+                <button> Continue with FaceBook</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
